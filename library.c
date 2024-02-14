68fbb3c3db2c7aff818622e6c1aca73b4c8874db
MichalKanas <159888367+MichalKanas@users.noreply.github.com>
2024-02-14 15:47:21 +0100

    library.c

--- library.c
+++ library.c
@@ -1,4 +1,10 @@
 /* Using API function gpio_toggle(): */
+	
+	void blink()
+	{
+
 		gpio_toggle(LEDPORT, LEDPIN);	/* LED on/off */
 		for (i = 0; i < 10000; i++) {	/* Wait a bit. */
 			__asm__("nop");
+			}
+			}
